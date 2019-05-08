1ed7019b99a2df9f9f0ebe4e6c87ae66c2cf70e8
wangzhongjie <uvdream@163.com>
2019-05-08 17:20:13 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
         <router-view />
-    <div>
-      <p>
-        If Ant-Design-Vue is successfully added to this project, you'll see an
-        <code v-text="'<a-button>'"></code>
-      </p>
-      <a-button type="primary">Primary</a-button>
-    </div>
   </div>
 </template>
 
@@ -23,8 +16,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
